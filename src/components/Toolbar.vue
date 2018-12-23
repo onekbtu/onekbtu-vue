9316c06b49f80a671c4eff0c6483427eeccc4964
NurlashKO <zh.nurlan96@gmail.com>
2018-12-24 01:52:14 +0600

    basic markup

---
+++ src/components/Toolbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-toolbar fixed app>
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+    <v-toolbar-title>One KBTU</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn flat>CREATE POST</v-btn>
+      <v-btn flat>FOLLOWED</v-btn>
+      <v-btn flat>PROFILE</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  name: 'Toolbar',
+};
+</script>
+
+<style scoped>
+
+</style>
