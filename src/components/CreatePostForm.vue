cc6dcd9b8edc10982acb912d581f994ca7a17b5e
muslimbeibytuly <muslimbeibytuly@gmail.com>
2019-01-20 18:34:26 +0600

    WIP Add CreatePost

---
+++ src/components/CreatePostForm.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id='CreatePostForm'>
+    <label>
+      <input type='text' name='title'>
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CreatePostForm',
+};
+</script>
+
+<style scoped>
+
+</style>
